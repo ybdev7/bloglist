7aed4dd81aaac81d3645fb21ba0d52e8f533cd7d
ybdev7 <99820567+ybdev7@users.noreply.github.com>
2024-02-12 10:47:54 -0500

    4.2 logger and config

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
-
+const { info, error } = require("./utils/logger");
+const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const cors = require("cors");
@@ -14,8 +15,8 @@ const blogSchema = new mongoose.Schema({
 
 const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGODB_URI;
-//"mongodb://localhost/bloglist";
+const mongoUrl = config.MONGODB_URI;
+
 mongoose.connect(mongoUrl);
 
 app.use(cors());
@@ -35,7 +36,6 @@ app.post("/api/blogs", (request, response) => {
   });
 });
 
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  info(`Server running on port ${config.PORT}`);
 });
