38895fbc155be3263afc399e95966f8e71fd0df1
ybdev7 <99820567+ybdev7@users.noreply.github.com>
2024-02-19 16:11:38 -0500

    4.17-4.19 added login with token auth and blogs POST with token auth

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const {
 
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 mongoose.connect(config.MONGODB_URI);
@@ -21,6 +22,7 @@ app.use(express.json());
 app.use(requestLogger);
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const getTokenFrom = (req) => {
+  const authorization = req.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (req, res) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -22,15 +31,21 @@ blogsRouter.get("/:id", async (req, res, next) => {
 });
 
 blogsRouter.post("/", async (req, res, next) => {
-  console.log(req.body.userId);
-  const user = await User.findById(req.body.userId);
-  console.log(user);
+  //console.log(req.body.userId);
+
+  const decodedToken = jwt.verify(getTokenFrom(req), process.env.SECRET);
+  if (!decodedToken || !decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
+
+  //console.log(user);
   const blog = new Blog({
     title: req.body.title,
     author: req.body.author,
     url: req.body.url,
     likes: req.body.likes,
-    user: user.id,
+    user: user._id,
   });
 
   const result = await blog.save();
