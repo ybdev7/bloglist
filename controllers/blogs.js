8580218b68e7b3db1894d1ff0695be833c7fdf5f
ybdev7 <99820567+ybdev7@users.noreply.github.com>
2024-02-19 12:11:32 -0500

    populate blogs with user (GET, GET/:id, POST) and populate users with blogs (GET)

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,14 +1,18 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (req, res) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
 
   res.json(blogs);
 });
 
 blogsRouter.get("/:id", async (req, res, next) => {
-  const blog = await Blog.findById(req.params.id);
+  const blog = await Blog.findById(req.params.id).populate("user", {
+    username: 1,
+    name: 1,
+  });
 
   if (blog) {
     res.json(blog);
@@ -18,9 +22,20 @@ blogsRouter.get("/:id", async (req, res, next) => {
 });
 
 blogsRouter.post("/", async (req, res, next) => {
-  const blog = new Blog(req.body);
+  console.log(req.body.userId);
+  const user = await User.findById(req.body.userId);
+  console.log(user);
+  const blog = new Blog({
+    title: req.body.title,
+    author: req.body.author,
+    url: req.body.url,
+    likes: req.body.likes,
+    user: user.id,
+  });
 
   const result = await blog.save();
+  user.blogs = user.blogs.concat(result._id);
+  user.save();
 
   res.status(201).json(result);
 });
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,12 @@ const usersRouter = require("express").Router();
 const User = require("../models/user");
 
 usersRouter.get("/", async (req, res) => {
-  const blogs = await User.find({});
+  const blogs = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  });
 
   res.json(blogs);
 });
